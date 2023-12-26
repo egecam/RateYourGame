5f4e0bd316900f4a418591f9dba4aab8161f6b40
yigitozman <108322277+yigitozman@users.noreply.github.com>
2023-12-26 23:04:26 +0300

    Fixed a little color problem (background was white when screen is not fully filled.)

--- app/src/main/java/com/example/rateyourgame/screens/GameDetailsScreen.kt
+++ app/src/main/java/com/example/rateyourgame/screens/GameDetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.rateyourgame.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,18 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.rateyourgame.R
 import com.example.rateyourgame.dataclasses.Game
 import com.example.rateyourgame.instances.RetrofitInstance
