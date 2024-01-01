bf3aba8cc2f4d5fe1cb961d3c1bbf9a96f912fb3
alperberkecelik <alp29emin34@gmail.com>
2024-01-01 19:08:45 +0300

    Added new login/signup screen layout

--- app/src/main/java/com/example/rateyourgame/screens/LoginScreen.kt
+++ app/src/main/java/com/example/rateyourgame/screens/LoginScreen.kt
@@ -1,14 +1,20 @@
 package com.example.rateyourgame.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,10 +26,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.example.rateyourgame.R
 import com.example.rateyourgame.ViewModels.AuthViewModel
 import com.example.rateyourgame.ViewModels.SharedViewModel
 import kotlinx.coroutines.launch
@@ -64,77 +75,117 @@ fun LoginScreen(navController: NavController,  authViewModel: AuthViewModel, sha
             }
         )
     }
+    val shapeis = if (isSystemInDarkTheme()) painterResource(id = R.drawable.loginshapenight) else painterResource(
+        id = R.drawable.loginshape)
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxWidth(),
     ) {
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Username") },
-            modifier = Modifier.fillMaxWidth().padding(8.dp)
-        )
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        if (showError) {
-            AlertDialog(
-                onDismissRequest = {
-                    showError = false
-                },
-                title = {
-                    Text(text = "Couldn't log in")
-                },
-                text = {
-                    Text("Username or Password is wrong. Try again.")
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            showError = false
-                        }
-                    ) {
-                        Text("OK")
-                    }
-                }
-            )
-        }
-        Button(
-            onClick = {
-                authViewModel.viewModelScope.launch {
-                    val user = authViewModel.login(username, password)
-                    if (user != null) {
-                        authViewModel.setUser(user)
-
-                        navController.navigate("game_list_screen")
-                    } else {
-                        showError = true
-                    }
-                }
-            },
-            modifier = Modifier
+        Box(
+            contentAlignment = Alignment.TopCenter, modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
         ) {
-            Text("Login")
+            Image(
+                painter = shapeis,
+                contentDescription = "loginshape",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillBounds
+            )
+
+            Image(
+                painterResource(id = R.drawable.rateyourgamelogo),
+                contentDescription = "rateyourgameloginlogo",
+                modifier = Modifier
+                    .size(225.dp)
+                    .padding(top = 50.dp),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("Don't have an account?")
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(
-            onClick = {
-                navController.navigate("signup_screen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Login", fontSize = 30.sp, fontWeight = FontWeight.Bold,)
+            Spacer(modifier = Modifier.height(15.dp))
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            if (showError) {
+                AlertDialog(
+                    onDismissRequest = {
+                        showError = false
+                    },
+                    title = {
+                        Text(text = "Couldn't log in")
+                    },
+                    text = {
+                        Text("Username or Password is wrong. Try again.")
+                    },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                showError = false
+                            }
+                        ) {
+                            Text("OK")
+                        }
+                    }
+                )
             }
-        ) {
-            Text("Sign up")
+
+            Button(
+                onClick = {
+                    authViewModel.viewModelScope.launch {
+                        val user = authViewModel.login(username, password)
+                        if (user != null) {
+                            authViewModel.setUser(user)
+
+                            navController.navigate("game_list_screen")
+                        } else {
+                            showError = true
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp)
+            )
+
+            {
+                Spacer(modifier = Modifier.height(30.dp))
+                Text("Login")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Don't have an account?")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    navController.navigate("signup_screen")
+                }
+            ) {
+                Text("Sign up")
+            }
         }
+
     }
 }
